e5ec163375734472d56d0ee1b44cbaa451deeea5
Maxwell Struever <maxstruever@gmail.com>
2015-04-30 08:23:53 -0400

    working with google map

--- js/app.js
+++ js/app.js
@@ -4,6 +4,34 @@
 		$scope.locdata = "Loading..";
 		$scope.locfound = false;
 		$scope.searching = false;
+		//set up google map var
+		$scope.googleMap = null;
+		//functiont to get the location
+		this.wpid = null;
+
+		this.geo_options = {
+			enableHighAccuracy: true, //get the highest accuracy possible (on mobiles, this uses the GPS when allowed)
+			maximumAge: Infinity, //gets the most recent location, regardless of when that was
+			timeout: 30000
+		};
+		//main function that gets the user's location
+		this.getLoc = function() {
+			if (navigator && navigator.geolocation) {
+				this.wpid = navigator.geolocation.watchPosition(this.geo_success, this.geo_error, this.geo_options);
+				$scope.searching = true;
+			}
+		};
+		this.initializeMap = function() {
+			var mapOptions = {
+				center: {
+					lat: -34.397,
+					lng: 150.644
+				},
+				zoom: 8
+			};
+			$scope.googleMap = new google.maps.Map(document.getElementById('map-canvas'),
+				mapOptions);
+		};
 		this.geo_success = function(position) {
 			//update the scope's new postion data from the new location 
 			$scope.locdata = position;
@@ -14,7 +42,16 @@
 			$("#compassarrow").css("transform", "rotate(" + direction + "deg)").css("-webkit-transform", "rotate(" + direction + "deg)").css("-mstransform", "rotate(" + direction + "deg)");
 			$scope.locfound = true;
 			$scope.searching = false;
-
+			//re-center the map on the coordinates
+			var googlePos = new google.maps.LatLng($scope.locdata.coords.latitude, $scope.locdata.coords.longitude);
+			$scope.googleMap.setCenter(googlePos);
+			//add a little box for it
+			var infowindow = new google.maps.InfoWindow({
+				map:$scope.googleMap,
+				position: googlePos,
+				content: 'Location found using HTML5.'
+			});
+			
 		};
 
 		this.geo_error = function(error) {
@@ -38,20 +75,10 @@
 			navigator.geolocation.clearWatch(this.wpid);
 		};
 
-		this.geo_options = {
-			enableHighAccuracy: true, //get the highest accuracy possible (on mobiles, this uses the GPS when allowed)
-			maximumAge: Infinity, //gets the most recent location, regardless of when that was
-			timeout: 30000
-		};
 
-		//functiont to get the location
-		this.wpid = null;
-		this.getLoc = function() {
-			if (navigator && navigator.geolocation) {
-				this.wpid = navigator.geolocation.watchPosition(this.geo_success, this.geo_error, this.geo_options);
-				$scope.searching = true;
-			}
-		};
+		//set up the google map
+		//google.maps.event.addDomListener(window, 'load', this.initializeMap);
+		this.initializeMap();
 		//call the get location function initially
 		this.getLoc();
 	});
